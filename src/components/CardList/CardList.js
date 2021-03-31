532bae9271696a71657e27c6e8e51a67ff30a94b
LimongiVirgil <limongi.virgil@gmail.com>
2021-03-31 15:42:19 +0200

    begin refacto of CardList: make infoCard components, merge neutral cards with factions cards => delete two logic to make one

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,70 +1,64 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './CardList.scss'
 import data from '../../cards.json'
 
 //Tools
 import { getFactionCards } from '../../tools/localStorageTools';
 
+//Components
+import InfoCards from '../InfoCards/InfoCards'
+
 function CardList(props) {
 	const [open, setOpen] = useState(false);
 	const [idc, setIdc] = useState(null);
-
-	const [openNeutre, setOpenNeutre] = useState(false);
-	const [idcNeutre, setIdcNeutre] = useState(null);
 
 	// Get faction and cards list
 	let factionName = props.faction
 	var existing = getFactionCards(factionName)
 
-	//var chiefCard = true;
-
 	// Add by default a king
-	props.kingData.forEach((id) => {
-		if (!existing.includes("202") && !existing.includes("203") && !existing.includes("204") && !existing.includes("205")) {
-			return (
-				existing.push("202")
-			)
-		}
-	})
+	if (props.kingData.every(king => !existing.includes(king))) {
+		existing.push(props.kingData[0])
+	}
 	localStorage.setItem(factionName, existing.toString());
+
+	// Merge faction cards list with neutral cards list
+	const cardsList = [...data.Neutre, ...props.data];
 
 	////// Display info Card //////
 
 	const infoCard = (e) => {
 		setIdc(e.target.id)
 		setOpen(!open)
+
 		e.preventDefault()
 	}
 
-	const infoCardNeutre = (e) => {
-		setIdcNeutre(e.target.id)
-		setOpenNeutre(!openNeutre)
-		e.preventDefault()
+	const closeInfoCard = (e) => {
+		setOpen(!open)
+		console.log('heelo')
 	}
 
 	//////// Register card in localhost ////////
 
 	const cardClick = (e) => {
-		// Get name of faction
-		let parentClass = e.target.parentNode.className.split(' ')[0];
-
 		// Get deck list
-		let deck = document.querySelector(`.mainDeck .${parentClass}.faction`)
+		let deck = document.querySelector(`.mainDeck .${factionName}.faction`)
 		//Get card list
-		let card = document.querySelector(`.deck .${parentClass}.faction`)
+		let card = document.querySelector(`.deck .${factionName}.faction`)
 
 		// Get the existing data
-		var existing = localStorage.getItem(parentClass);
+		var existing = localStorage.getItem(factionName);
 
 		// If no existing data, create an array
 		// Otherwise, convert the localStorage string to an array
 		existing = existing ? existing.split(',') : [];
 
-		var unite = document.querySelector(`.${parentClass} .unites`)
-		var powerUnite = document.querySelector(`.${parentClass} .power`)
-		var heroe = document.querySelector(`.${parentClass} .count`)
-		var cardUnite = document.querySelector(`.${parentClass} .existing`)
-		var special = document.querySelector(`.${parentClass} .speciales`)
+		var unite = document.querySelector(`.${factionName} .unites`)
+		var powerUnite = document.querySelector(`.${factionName} .power`)
+		var heroe = document.querySelector(`.${factionName} .count`)
+		var cardUnite = document.querySelector(`.${factionName} .existing`)
+		var special = document.querySelector(`.${factionName} .speciales`)
 
 		var separator = special.innerHTML.indexOf(' ');
 
@@ -79,36 +73,10 @@ function CardList(props) {
 			}
 
 			// Save back to localStorage
-			localStorage.setItem(parentClass, existing.toString());
+			localStorage.setItem(factionName, existing.toString());
 
 			// Add 1 to infocard unit when a card is choose
-			props.data.map((card) => {
-				if (e.target.id === card.id.toString()) {
-					if (!card.special && card.effect1 !== "heroe" && card.effect1 !== "king") {
-						unite.innerHTML = Number(unite.innerHTML) - 1;
-					}
-					if (card.effect1 === "heroe") {
-						heroe.innerHTML = Number(heroe.innerHTML) - 1
-					}
-					if (card.special) {
-						special.innerHTML = Number(special.innerHTML.substr(0, separator)) - 1 + " / 10"
-					}
-					powerUnite.innerHTML = Number(powerUnite.innerHTML) - card.power
-					cardUnite.innerHTML = Number(cardUnite.innerHTML) - 1
-				}
-
-				if (special.innerHTML === "10 / 10") {
-					special.style.color = "red"
-				} else {
-					special.style.color = "white"
-				}
-				if (cardUnite.innerHTML === '40') {
-					cardUnite.style.color = 'red'
-				} else {
-					cardUnite.style.color = "white"
-				}
-			})
-			data.Neutre.map((card) => {
+			cardsList.forEach((card) => {
 				if (e.target.id === card.id.toString()) {
 					if (!card.special && card.effect1 !== "heroe" && card.effect1 !== "king") {
 						unite.innerHTML = Number(unite.innerHTML) - 1;
@@ -136,7 +104,7 @@ function CardList(props) {
 			})
 		} else {
 			if (special.innerHTML === "10 / 10") {
-				data.Neutre.map((card) => {
+				data.Neutre.forEach((card) => {
 					if (e.target.id === card.id.toString() && (card.effect1 === "meteo" || card.effect1 ===  "chargeRing" || card.effect1 ===  "destroy" || card.effect1 ===  "lure")) {
 						addCard = false;
 					}
@@ -150,39 +118,13 @@ function CardList(props) {
 					existing.push(e.target.id);
 
 					// Save back to localStorage
-					localStorage.setItem(parentClass, existing.toString());
+					localStorage.setItem(factionName, existing.toString());
 
 					// Add select card to deck list
 					deck.append(e.target)
 
 					// Add 1 to infocard unit when a card is choose
-					props.data.map((card) => {
-						if (e.target.id === card.id.toString()) {
-							if (!card.special && card.effect1 !== "heroe" && card.effect1 !== "king") {
-								unite.innerHTML = Number(unite.innerHTML) + 1;
-							}
-							if (card.effect1 === "heroe") {
-								heroe.innerHTML = Number(heroe.innerHTML) + 1
-							}
-							if (card.special) {
-								special.innerHTML = Number(special.innerHTML.substr(0, separator)) + 1 + " / 10"
-							}
-							powerUnite.innerHTML = Number(powerUnite.innerHTML) + card.power
-							cardUnite.innerHTML = Number(cardUnite.innerHTML) + 1
-						}
-
-						if (special.innerHTML === "10 / 10") {
-							special.style.color = "red"
-						} else {
-							special.style.color = "white"
-						}
-						if (cardUnite.innerHTML === '40') {
-							cardUnite.style.color = 'red'
-						} else {
-							cardUnite.style.color = "white"
-						}
-					})
-					data.Neutre.map((card) => {
+					cardsList.forEach((card) => {
 						if (e.target.id === card.id.toString()) {
 							if (!card.special && card.effect1 !== "heroe" && card.effect1 !== "king") {
 								unite.innerHTML = Number(unite.innerHTML) + 1;
@@ -216,52 +158,21 @@ function CardList(props) {
 	}
 
 	return(
-		<div active={props.active} className={props.faction + " faction"}>
-			{data.Neutre.map((card, index) =>
-				existing.includes(card.id.toString()) ? null : <img key={index} id={card.id} onDoubleClick={cardClick} onContextMenu={infoCardNeutre} src={card.image_url} alt="carte" />
+		<div active={props.active} className={`${props.faction} faction`}>
+			{cardsList.map((card, index) =>
+				(existing.includes(card.id.toString()) || card.effect1 === "king") ? 
+					null : 
+					<img 
+						key={index} 
+						id={card.id} 
+						onDoubleClick={cardClick} 
+						onContextMenu={infoCard} 
+						src={card.image_url} 
+						alt="carte"
+					/>
 			)}
-			{props.data.map((card, index) =>
-				(existing.includes(card.id.toString()) || card.effect1 === "king") ? null : <img key={index} id={card.id} onDoubleClick={cardClick} onContextMenu={infoCard} src={card.image_url} alt="carte" />
-			)}
-			{open === true &&
-				props.data.map((card, index) => {
-					if (card.id.toString() === idc) {
-						return( 
-							<div key={index} onClick={() => setOpen(!open)} className="infoCard">
-								<img src={card.image_url} alt="carte" />
-								<div href="#" className="close" />
-								{ card.description1 &&
-									<div className="infoCardText">
-										{card.nameEffect && <p>{card.nameEffect}</p>}
-										{card.description1 && <p>{card.description1}</p>}
-										{card.description2 && <p>{card.description2}</p>}
-									</div>
-								}
-							</div>
-						)
-					}
-					return false
-				})
-			}
-			{openNeutre === true &&
-				data.Neutre.map((card, index) => {
-					if (card.id.toString() === idcNeutre) {
-						return( 
-							<div key={index} onClick={() => setOpenNeutre(!openNeutre)} className="infoCard">
-								<img src={card.image_url} alt="carte" />
-								<div href="#" className="close" />
-								{ card.description1 &&
-									<div className="infoCardText">
-										{card.nameEffect && <p>{card.nameEffect}</p>}
-										{card.description1 && <p>{card.description1}</p>}
-										{card.description2 && <p>{card.description2}</p>}
-									</div>
-								}
-							</div>
-						)
-					}
-					return false
-				})
+			{ open === true && 
+				<InfoCards cards={cardsList} idCard={idc} closeInfo={closeInfoCard}/>
 			}
 		</div>
 	)
---
+++ src/components/InfoCards/InfoCards.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function InfoCards({closeInfo, idCard, cards}) {
+
+	return (
+		<>
+			{cards.map((card, index) => {
+				if (card.id.toString() === idCard) {
+					return( 
+						<div key={index} onClick={closeInfo} className="infoCard">
+							<img src={card.image_url} alt="carte" />
+							<div href="#" className="close" />
+							{ card.description1 &&
+								<div className="infoCardText">
+									{card.nameEffect && <p>{card.nameEffect}</p>}
+									{card.description1 && <p>{card.description1}</p>}
+									{card.description2 && <p>{card.description2}</p>}
+								</div>
+							}
+						</div>
+					)
+				}
+				return false
+			})}
+		</>
+	)
+}
